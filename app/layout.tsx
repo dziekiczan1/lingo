a837094c3ae8dd1dba52912b62ffc06c9b008a00
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-05-29 21:53:48 +0200

    add clerk middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -24,15 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
