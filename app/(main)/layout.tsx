10b1fd98bc2c981129f503d835ea390bb30f3e52
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-01 19:40:49 +0200

    add sidebar component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,10 @@
+import { Sidebar } from "@/components/sidebar";
 import type { PropsWithChildren } from "react";
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <div className="hidden lg:flex" />
+      <Sidebar className="hidden lg:flex" />
       <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
         <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
       </main>
---
+++ components/sidebar.tsx
@@ -0,0 +1,7 @@
+type SidebarProps = {
+  className?: string;
+};
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  return <div className="bg-blue-500 h-full w-[256px]">Sidebar</div>;
+};
