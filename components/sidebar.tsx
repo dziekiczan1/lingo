f5297d340c8da989e14f4d28125612b362138fbd
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-02 11:02:27 +0200

    add content to sidebar component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,8 @@
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 import { cn } from "@/lib/utils";
 
 type SidebarProps = {
@@ -8,11 +13,36 @@ export const Sidebar = ({ className }: SidebarProps) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "left-0 top-0 flex h-full flex-col border-r-2 px-4 lg:fixed lg:w-[256px]",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+          <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
+
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+
+      <div className="flex flex-1 flex-col gap-y-2">SidebarItem</div>
+
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { userButtonPopoverCard: { pointerEvents: "initial" } },
+            }}
+          />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
