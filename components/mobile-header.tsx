fc1a49148121fe8596d1767734432fed1f453190
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-02 10:23:52 +0200

    add mobile header component

---
+++ components/mobile-header.tsx
@@ -0,0 +1,7 @@
+export const MobileHeader = () => {
+  return (
+    <nav className="fixed top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-4 lg:hidden">
+      MobileSidebar
+    </nav>
+  );
+};
