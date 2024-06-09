797f7598bb06575a84c8ed19f1d971f063cc55ac
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-09 17:43:05 +0200

    add userprogress schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,19 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { MAX_HEARTS } from "@/constants";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(), // serial auto-increments the id - can use uuid as well
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
+});
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(MAX_HEARTS),
+  points: integer("points").notNull().default(0),
 });
