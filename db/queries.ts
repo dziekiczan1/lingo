21a11a95cc8768ccb9432917b74c9584c1a12d2a
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-10 19:12:11 +0200

    add getuserprogress db query

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,27 @@
 import { cache } from "react";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+
+import { userProgress } from "./schema";
 import db from "./drizzle";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
 
   return data;
 });
