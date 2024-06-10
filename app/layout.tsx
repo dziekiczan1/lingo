fcb9e216dfbdb256fc4c6fcfb340f163345173ad
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-10 19:41:36 +0200

    add toaster component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Toaster theme="light" richColors closeButton />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
