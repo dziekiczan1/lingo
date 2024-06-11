0a0cd561201ece0de4be96394abd829b1471b80f
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-11 16:52:10 +0200

    install tsx package, add seed script

---
+++ scripts/prod.ts
@@ -0,0 +1,36 @@
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "@/db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding database");
+
+    // Delete all existing data
+    await Promise.all([
+      db.delete(schema.userProgress),
+      db.delete(schema.courses),
+    ]);
+
+    await db.insert(schema.courses).values([
+      { id: 1, title: "Spanish", imageSrc: "/es.svg" },
+      { id: 2, title: "Italian", imageSrc: "/it.svg" },
+      { id: 3, title: "French", imageSrc: "/fr.svg" },
+      { id: 4, title: "Croatian", imageSrc: "/hr.svg" },
+      { id: 5, title: "Japan", imageSrc: "/jp.svg" },
+    ]);
+
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
+
+void main();
