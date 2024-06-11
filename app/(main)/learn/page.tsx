fe908877b9e0ed9d3553151a9203c8bceee18651
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-11 16:56:39 +0200

    replace static data with dynamic

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -16,14 +16,14 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Test", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Test" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { courses } from "@/db/schema";
 
 type UserProgressProps = {
-  activeCourse: any;
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
