d54828bfa080459eedad5fe65d1e0e458ddfaef5
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-06-23 15:46:16 +0200

    add quests component

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { FeedWrapper } from "@/components/feed-wrapper";
-import { Quests } from "@/components/quests";
+
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { getUserProgress, getUserSubscription } from "@/db/queries";
 import { Items } from "./items";
+import { Quests } from "@/components/quests";
 
 const ShopPage = async () => {
   const userProgressData = getUserProgress();
